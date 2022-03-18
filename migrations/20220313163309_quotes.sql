6eb40ee72d4bf839898066251cd7070b7cebc2c3
redhairone <38636926+redhairone@users.noreply.github.com>
2022-03-18 20:04:31 +0100

    Update 20220313163309_quotes.sql

--- migrations/20220313163309_quotes.sql
+++ migrations/20220313163309_quotes.sql
@@ -1,5 +1,5 @@
 -- Add migration script here
 CREATE TABLE Quotes (
-    ID int NOT NULL PRIMARY KEY,
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, //AUTOINCREMENT will automatically assign the value so you dont have to do it
     Quote TEXT NOT NULL
 )
